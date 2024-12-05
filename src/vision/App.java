d8f5e108e80d4947500dbaa22b4a360a21d6d171
cibellyhnb <cibellyhnb@gmail.com>
2024-12-05 15:26:34 -0300

    todolist

--- src/vision/App.java
+++ src/vision/App.java
@@ -4,8 +4,9 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+
 
 public class App extends Application{
 
